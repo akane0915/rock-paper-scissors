90152cd495f26a624e5f00af36906b4f0a6e649d
Asia Kane and Ken Rutan <asia@>
2017-04-19 10:31:34 -0700

    Add a set of functions for identical player inputs.

--- lib/rps.rb
+++ lib/rps.rb
@@ -4,6 +4,17 @@ class String
       output = "Player 1 Wins!"
     elsif self == "rock" && player2 == "rock"
       output = "Draw!"
+
+    elsif self == "paper" && player2 == ""
+        output = "Player 1 Wins!"
+    elsif self == "paper" && player2 == "paper"
+      output = "Draw!"
+
+
+    elsif self == "scissors" && player2 == ""
+        output = "Player 1 Wins!"
+    elsif self == "scissors" && player2 == "scissors"
+      output = "Draw!"
     end
     output
   end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -3,12 +3,20 @@ require('rps')
 require('pry')
 
 describe("String#rockps") do
-  it("takes player 1 input and returns the outcome player 1 wins") do
+  it("takes player 1 input as rock and no input from player two returns the outcome player 1 wins") do
     expect('rock'.rockps('')).to(eq('Player 1 Wins!'))
   end
 
-  it("takes both user inputs and returns draw") do
+  it("takes player 1 input as paper and no input from player two and returns the outcome player 1 wins") do
+    expect('paper'.rockps('')).to(eq('Player 1 Wins!'))
+  end
+
+  it("takes both user inputs as rock and returns a draw") do
     expect('rock'.rockps('rock')).to(eq('Draw!'))
+  end
+
+  it("takes both user inputs as scissors and returns a draw") do
+    expect('scissors'.rockps('scissors')).to(eq('Draw!'))
   end
 
 end

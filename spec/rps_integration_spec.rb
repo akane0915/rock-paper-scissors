6161fefa1238174e8ed9454e9469df1fefc464f7
Asia Kane and Ken Rutan <asia@>
2017-04-19 13:31:46 -0700

    capybara integration test passes

--- spec/rps_integration_spec.rb
+++ spec/rps_integration_spec.rb
@@ -6,9 +6,19 @@ set(:show_exceptions, false)
 describe('The rock, paper, scissors path', {:type => :feature}) do
   it('Processes two user inputs and returns the winner or draw conditions.') do
     visit('/')
-    fill_in('player1', :with => 'rock')
-    fill_in('player2', :with => 'scissors')
+
+    # choose('rock')
+    # This finds two elements.
+
+    # fill_in('player1', :with => 'rock')
+    # fill_in('player2', :with => 'scissors')
+    # fill_in([locator], options = {}) ⇒ Object
+    # choose([locator], options) ⇒ Object
+    choose('rockp1')
+    choose('scissorsp2')
+
     click_button("Let's Play!")
+
     expect(page).to have_content('Player 1 Wins!')
   end
 end
